599fefb2a210793612101d071646c640cb5b70d7
LookAyd <79665260+LookAyd@users.noreply.github.com>
2021-02-28 02:15:07 +0300

    Revert "13 Şubat 1. Commit"
    This reverts commit 1d7694acc7ebcbd0e20b94a1b10ad2a8b7b1434d.

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -15,7 +15,5 @@ namespace Business.Abstract
         IDataResult<List<ProductDetailDto>> GetProductDetails();
         IDataResult<Product> GetById(int productId);
         IResult Add(Product product);
-
-        //RESTFUL --> HTTP --> 
     }
 }
